d30fcfd6b6059af5d994638576144d404ee5451e
Spolpinka <a421243266@gmail.com>
2023-06-21 12:11:59 +0300

    Initial commit

---
+++ src/main/java/pro/sky/studentssql/StudentsSqlApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.studentssql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/studentssql/StudentsSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.studentssql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
